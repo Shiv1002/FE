e0f89da403da04ee338373ef32caf7d7c367bd6b
Shiv1002 <85022318+Shiv1002@users.noreply.github.com>
2021-06-01 18:46:59 +0530

    finding armstrong no. in range of 100 to 1000

--- finding armstrong.c
+++ finding armstrong.c
@@ -2,27 +2,25 @@
 #include<math.h>
 int main()
 {
-int i,num,arm1,arm2,arm3;
-for(i=100;i<=999;i++)
-{
-    num=i;
-   if(num%100!=0)
+   int i,num,arm1,arm2,arm3;
+   for(i=100;i<=999;i++)
    {
-       arm1=pow((num%10),3);
-       num=num/10;
-       arm2=pow((num%10),3);
-       num=num/10;
-       arm3=pow((num%10),3);
-       {
-           if((arm1+arm2+arm3)==i)
+        num=i;
+        if(num%100!=0)
            {
-               printf("%d is armstrong\n",i);
+                  arm1=pow((num%10),3);
+                  num=num/10;
+                  arm2=pow((num%10),3);
+                  num=num/10;
+                  arm3=pow((num%10),3);
+                       {
+                           if((arm1+arm2+arm3)==i)
+                                {
+                                    printf("%d is armstrong\n",i);
+                                }
+                       }
 
-            }
+          }
 
-       }
-
-   }
-
-}
+    }
 }
