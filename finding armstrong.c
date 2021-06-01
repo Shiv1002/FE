ca361871279dfc3e431b6c1a0a6686aa388e63c1
Shiv1002 <85022318+Shiv1002@users.noreply.github.com>
2021-06-01 13:20:39 +0530

    Add files via upload
    simple program to find armstrong no. between 100 and 500

---
+++ finding armstrong.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int i,num,arm1,arm2,arm3;
+for(i=100;i<=999;i++)
+{
+    num=i;
+   if(num%100!=0)
+   {
+       arm1=pow((num%10),3);
+       num=num/10;
+       arm2=pow((num%10),3);
+       num=num/10;
+       arm3=pow((num%10),3);
+       {
+           if((arm1+arm2+arm3)==i)
+           {
+               printf("%d is armstrong\n",i);// not taking no. 153
+
+            }
+
+       }
+
+   }
+
+}
+}
